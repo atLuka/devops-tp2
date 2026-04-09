fa9c8509d7527501d4bc405482cf62487717a32a
Luka <luka.samac@outlook.fr>
2026-04-09 10:14:34 +0200

    Ajout service CarService

---
+++ src/main/java/tp1/tp1/services/CarService.java
@@ -0,0 +1,26 @@
+package tp1.tp1.services;
+
+import tp1.tp1.entities.Car;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+    private List<Car> cars = new ArrayList<>();
+
+    public void addCar(Car car) {
+        cars.add(car);
+    }
+
+    public Car getCar(String plateNumber) {
+        return cars.stream()
+                .filter(car -> car.getPlateNumber().equals(plateNumber))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Car> getCars() {
+        return new ArrayList<>(cars);
+    }
+}
